6b5d11497bf8b73374e47dd432c95a038b1e8807
joelcaminas02 <joelcg2002@gmail.com>
2021-09-27 09:07:50 +0200

    Acabados los 3 primeros ejemplos del tema 1

---
+++ tema 1/Ejemplo2.java
@@ -0,0 +1,9 @@
+public class Ejemplo2 {
+	public static void main(String[] args){
+	int dividendo = 20;
+	int divisor = 6;
+	int sumaAlFinal = 3;
+
+	System.out.println("El resultado es: "+((dividendo/divisor) + sumaAlFinal));
+}
+}
---
+++ tema 1/Ejercicio3.java
@@ -0,0 +1,6 @@
+public class Ejercicio3{
+	public static void main(String[] args){
+	int squareArea = (5*5);
+	System.out.println("El resultado es: " + squareArea);
+}
+}
