e8a734d29a9d044391e6314b76b8fe52ea291c2d
joelcaminas02 <joelcg2002@gmail.com>
2021-09-29 11:00:28 +0200

    ejercicio6

---
+++ tema 1/ejercicio6.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ejercicio6 {
+    public static void main(String[] args) {
+    System.out.println("Introduce el radio: ");
+    Scanner sc = new Scanner(System.in);
+    double radio = sc.nextDouble();
+    double PI = 3.14159;
+    System.out.println("La longitud del circulo es: "+ (2*radio)*PI);
+    System.out.println("El area del circulo es: "+ (radio*radio)*PI);
+    }
+}
