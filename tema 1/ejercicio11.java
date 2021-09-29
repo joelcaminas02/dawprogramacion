b8f85ccf70b2f620fd2739028a8ab6d9d3ef8425
joelcaminas02 <joelcg2002@gmail.com>
2021-09-29 13:42:54 +0200

    ejercicio12

---
+++ tema 1/ejercicio11.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class ejercicio11 {
+    public static void main(String[] args) {
+        System.out.println("Introduce un valor ");
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        int v1 = inputValue.nextInt();
+        
+        System.out.println("Introduce el segundo valor");
+        inputValue = new Scanner(System.in);
+        int v2 = inputValue.nextInt();
+
+        if ( v1 > v2 ){
+            System.out.println(v1);
+            System.out.println(v2);
+        }else{
+            System.out.println(v2);
+            System.out.println(v1);
+           }
+           inputValue.close();
+    }
+}
---
+++ tema 1/ejercicio12.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class ejercicio12 {
+    public static void main(String[] args) {
+        System.out.println("Introduce un valor ");
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        int v1 = inputValue.nextInt();
+        
+        System.out.println("Introduce el segundo valor");
+        inputValue = new Scanner(System.in);
+        int v2 = inputValue.nextInt();
+
+        if ( v1 > v2 ){
+            System.out.println(v1);
+        }else{
+            System.out.println(v2);
+           }
+           inputValue.close();
+    }
+}
