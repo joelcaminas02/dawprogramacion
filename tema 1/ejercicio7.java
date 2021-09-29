ccb0c610de2709adf3a8a327b5ac0c191e9c0328
joelcaminas02 <joelcg2002@gmail.com>
2021-09-29 13:00:46 +0200

    ejercicio7acabado

--- tema 1/ejercicio7.java
+++ tema 1/ejercicio7.java
@@ -1,9 +1,15 @@
-import java.util
-
 import java.util.*;
 public class ejercicio7 {
     public static void main(String[] args) {
-        
+        System.out.println("Introduce el precio real: ");
+        Scanner sc = new Scanner(System.in);
+        double pr = sc.nextDouble();
+
+        System.out.println("Introduce el precio rebajado");
+        double po = sc.nextDouble();
+
+        System.out.println("El descuente es de un: "+(pr-po)*100/pr
+        +"%");
     }
     
 }
