649884e93fb8a6896b45a40a00f5c1fd95aff941
joelcaminas02 <joelcg2002@gmail.com>
2021-10-01 09:52:58 +0200

    Ejercicio

---
+++ tema 1/Ejercicio11.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Ejercicio11 {
+    public static void main(String[] args) {
+        System.out.println("Introduce un valor ");
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        int v1 = inputValue.nextInt();
+        
+        System.out.println("Introduce el segundo valor");
+        int v2 = inputValue.nextInt();
+        
+        if ( v1 > v2 ){
+            System.out.println(v1);
+            System.out.println(v2);
+        }else{
+            System.out.println(v2);
+            System.out.println(v1);
+           }
+        inputValue.close();
+    }  
+}
