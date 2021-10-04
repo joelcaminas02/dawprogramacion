6d9547f63e270a290804c0de342a43113069640c
joelcaminas02 <joelcg2002@gmail.com>
2021-10-04 12:10:33 +0200

    ejercicio16

--- tema 1/Ejercicio16.java
+++ tema 1/Ejercicio16.java
@@ -1,35 +1,39 @@
 import java.util.Scanner;
+
 public class Ejercicio16 {
     public static void main(String[] args) {
-    Scanner inputValue;
-    inputValue = new Scanner(System.in);
-    String op;
+        
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        String op;
 
-    System.out.println("Introduce un valor: ");
-    int v1 = inputValue.nextInt();
-    System.out.println("Introduce otro valor: ");
-    int v2 = inputValue.nextInt();
-    System.out.println("Indica la operacion que deseas realizar");
-    Scanner miScanner = new Scanner(System.in);
-    op = miScanner.next();
+        System.out.println("Introduce un valor: ");
+        int v1 = inputValue.nextInt();
+        System.out.println("Introduce otro valor: ");
+        int v2 = inputValue.nextInt();
+        System.out.println("Indica la operacion que deseas realizar");
+        Scanner miScanner = new Scanner(System.in);
+        op = miScanner.next();
 
-    switch (op) {
-        case "+":
-            System.out.println("La suma de "+v1+"+"+v2+"=");
-            break;
-        case "-":
-            
-            break;
-        case "*":
-            
-            break;
-        case "/":
-
-            break;
-        default:
-            break;
+        switch (op) {
+            case "+":
+                System.out.println("La suma de "+v1+"+"+v2+"="+(v1+v2));
+                break;
+            case "-":
+                System.out.println("La resta de "+v1+"-"+v2+"="+(v1-v2));
+                break;
+            case "*":
+                System.out.println("La multiplicacion de "+v1+"*"+v2+"="+(v1*v2));
+                break;
+            case "/":
+                System.out.println("La division de "+v1+"/"+v2+"="+(v1/v2));
+                break;
+            default:
+                break;
+                System.out.println("Error");
     }
-
+    inputValue.close();
+    miScanner.close();
     }
 
     
