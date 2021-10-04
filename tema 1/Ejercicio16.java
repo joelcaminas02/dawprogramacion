9d51d81732dfa2989e032b9f4cac21381cf9ff44
joelcaminas02 <joelcg2002@gmail.com>
2021-10-04 10:09:01 +0200

    ejercicio164

--- tema 1/Ejercicio16.java
+++ tema 1/Ejercicio16.java
@@ -3,25 +3,27 @@ public class Ejercicio16 {
     public static void main(String[] args) {
     Scanner inputValue;
     inputValue = new Scanner(System.in);
-    
+    String op;
+
     System.out.println("Introduce un valor: ");
     int v1 = inputValue.nextInt();
     System.out.println("Introduce otro valor: ");
     int v2 = inputValue.nextInt();
     System.out.println("Indica la operacion que deseas realizar");
-    String operacion = inputValue.next(operacion);
+    Scanner miScanner = new Scanner(System.in);
+    op = miScanner.next();
 
-    switch (operacion) {
-        case suma:
+    switch (op) {
+        case "+":
             System.out.println("La suma de "+v1+"+"+v2+"=");
             break;
-        case resta:
+        case "-":
             
             break;
-        case multiplicacion:
+        case "*":
             
             break;
-        case division:
+        case "/":
 
             break;
         default:
