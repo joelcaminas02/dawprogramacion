cb168d713c21be07c7e2112235e0ec67f1dd9c37
joelcaminas02 <joelcg2002@gmail.com>
2021-10-08 08:49:13 +0200

    Ejercicio19 Finalizado

--- tema 1/Ejercicio19.java
+++ tema 1/Ejercicio19.java
@@ -1,42 +1,46 @@
 import java.util.*;
 public class Ejercicio19 {
     public static void main(String[] args) {
-        
+        //Declaracion variables
+        int horas, horasExtra;
+        double euros, bruto;
         Scanner lector;
         lector = new Scanner(System.in);
 
+        //Inicio programa
         System.out.println("Introduce cuanto te pagan por hora: ");
-        double euros = lector.nextDouble();
+        euros = lector.nextDouble();
 
         System.out.println("Introduce el numero de horas trabajado: ");
-        int horas = lector.nextInt();
+        horas = lector.nextInt();
 
         lector.close(); 
         
-        int horasExtra= horas - 35;
-        double calcEuros = euros*horas;
-        double calcExtra = (euros*1.5)*horasExtra;
-        double EurosSueldo= calcEuros+calcExtra;
-        
-        if ( EurosSueldo <= 500) {
-            System.out.println("Cobrara "+EurosSueldo+" este mes");
-        }else if ( EurosSueldo <= 900){
-            double impuestos = EurosSueldo-500;
+        if ( horas <= 35 ){
+            bruto = euros*horas;
+        }else{
+            horasExtra = horas-35;
+            bruto = ((euros*1.5)*horasExtra+euros*horas);
+        }
+            if ( bruto <= 500) {
+            System.out.println("Cobrara "+bruto+" este mes");
+            }else if ( bruto <= 900){
+            double impuestos = bruto-500;
             double impuestos1 = (impuestos*0.25);
-            double sueldo = EurosSueldo-impuestos1;
-            System.out.println("Cobrara bruto "+EurosSueldo+" este mes"); 
+            double sueldo = bruto-impuestos1;
+            System.out.println("Cobrara bruto "+bruto+" este mes"); 
             System.out.println("Cobrara neto "+sueldo+" este mes");
             System.out.println("De los impuestos se le quitara "+impuestos1);
-        }else if (EurosSueldo > 900){
-            double impuestos = EurosSueldo-900;
+            }else if (bruto > 900){
+            double impuestos = bruto-900;
             double impuestos25 = 400*0.25;
             double impuestos45 = (impuestos*0.45);
-            double sueldo = EurosSueldo-impuestos45-impuestos25;
-            System.out.println("Cobrara en bruto "+EurosSueldo+" este mes"); 
+            double sueldo = bruto-impuestos45-impuestos25;
+            System.out.println("Cobrara en bruto "+bruto+" este mes"); 
             System.out.println("Cobrara neto "+sueldo+" este mes");
             System.out.println("De los impuestos se le quitara un 25% es decir "+impuestos25+" y un 45% que son "+impuestos45);
-        }else{
+            }else{
             System.out.println("error");
-        }
+            }
     }
 }
