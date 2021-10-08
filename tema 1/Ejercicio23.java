443933865a3ebf37bfb77766758941dbf34ff1c0
joelcaminas02 <joelcg2002@gmail.com>
2021-10-08 10:20:22 +0200

    Ejercicio23 Acabado

--- tema 1/Ejercicio23.java
+++ tema 1/Ejercicio23.java
@@ -1,26 +1,25 @@
 import java.util.*;
-
-public class Ejercicio22 {
-  public static void main(String[] args) {
+public class Ejercicio23 {
+    public static void main(String[] args) {
       
         //Declaracion de variables
-        int numero, i, positives;
+        int positives, num;
         Scanner lector;
         lector = new Scanner(System.in);
 
         //Programa principal
         System.out.println("Introduce un numero: ");
+        num = lector.nextInt();
+
         
-        i = 0;
         positives = 0;
-        while (i < 10){
+        while (num != 0){
             
-            numero = lector.nextInt();
+            num = lector.nextInt();
             
-            if (numero >= 0){
+            if (num >= 0){
                 positives = positives + 1;
             }
-        i = i + 1;
         }    
         lector.close();
         System.out.println("Hay "+positives+" numeros positivos");
