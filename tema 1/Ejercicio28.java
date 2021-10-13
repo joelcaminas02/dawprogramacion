f76919198728a589ae55aa14a40afc177959cfba
joelcaminas02 <joelcg2002@gmail.com>
2021-10-13 10:44:15 +0200

    Ejercicio25

---
+++ tema 1/Ejercicio28.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Ejercicio28 {
+    public static void main(String[] args) {
+        //Declaracion de variables
+        int numero, divisor;
+        Scanner lector;
+        lector = new Scanner(System.in);
+        divisor = 1;
+
+        //Programa principal
+        System.out.println("Introduce el numero que desees averiguar si es primo: ");
+        numero = lector.nextInt();
+
+        lector.close();
+        
+        if (numero % divisor == 0){
+            System.out.println("Es primo");
+            }else{
+                System.out.println("No es primo");
+            }
+    }
+}
