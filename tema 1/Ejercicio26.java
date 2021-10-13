735a65520193d020fae5f135c8ee3318957016df
joelcaminas02 <joelcg2002@gmail.com>
2021-10-13 10:06:46 +0200

    Ejercicio26 Acabado

--- tema 1/Ejercicio26.java
+++ tema 1/Ejercicio26.java
@@ -3,18 +3,21 @@ import java.util.*;
 public class Ejercicio26 {
     public static void main(String[] args) {
         //Declaracion de variables
-        int multiplo, numero, i;
+        int multiplo, numero;
+        multiplo= 0;
         Scanner lector;
         lector = new Scanner(System.in);
 
         //Programa principal
         System.out.println("Introduce el numero que quieras saber la tabla de multiplicar: ");
         numero = lector.nextInt();
+        
+        lector.close();
 
-        i = 0;
-        do
-        while (numero >= 0) {
-
+        while ((multiplo >=0) && (multiplo <= 10)){
+            int tabla = multiplo * numero;
+            System.out.println(numero+"*"+multiplo+"="+tabla);
+            multiplo ++;
         }
-    }
+}
 }
