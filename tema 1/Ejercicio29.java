0ca7e10bf7e04e244f87bf24faa10dcef6eaf9f6
joelcaminas02 <joelcg2002@gmail.com>
2021-10-18 09:50:13 +0200

    29

--- tema 1/Ejercicio29.java
+++ tema 1/Ejercicio29.java
@@ -10,12 +10,16 @@ public class Ejercicio29 {
         System.out.println("Estos son los numeros primos del 3 al 100 ");
         
         for(int i=3;i<=100;i++){
+            divisor= 2;
+            esPrimo = true;
             while ((divisor < i) && esPrimo ){
                 if (i % divisor == 0){
                 esPrimo = false;
                 }
-                System.out.println(i);
-                i++;
+            divisor++;
+            }
+            if(esPrimo){
+            System.out.println(i);
         }
     }
     }    
