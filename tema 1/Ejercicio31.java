f878793d3f8e6f9934962fc125933747ceff7d8f
joelcaminas02 <joelcg2002@gmail.com>
2021-10-20 08:37:45 +0200

    Ejercicio31

---
+++ tema 1/Ejercicio31.java
@@ -0,0 +1,37 @@
+public class Ejercicio31 {
+    
+    public static void main(String[] args) {
+        //declares an array of integers
+        int [] anArray;
+
+        //alocates memory for 10 integers
+        anArray = new int[10];
+
+        //initialize first element
+        anArray[0] = 100;
+
+        //initialize second element
+        anArray[1]= 200;
+
+        //and so forth
+        anArray[2] = 300;
+
+        anArray[3] = 400;
+
+        anArray[4] = 500;
+
+        anArray[5] = 600;
+
+        anArray[6] = 700;
+
+        anArray[7] = 800;
+
+        anArray[8] = 900;
+
+        anArray[9] = 1000;
+
+        for (int i= 0; i<=9;i++){
+            System.out.println(anArray[i]);
+        }
+    }
+}
