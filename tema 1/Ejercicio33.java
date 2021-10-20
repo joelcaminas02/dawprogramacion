ebbb82825bb35fa9c7cf1dbb2f5c87707bfc2537
joelcaminas02 <joelcg2002@gmail.com>
2021-10-20 10:15:44 +0200

    Ejercicio33

--- tema 1/Ejercicio33.java
+++ tema 1/Ejercicio33.java
@@ -2,7 +2,18 @@ import java.util.*;
 
 public class Ejercicio33 {
     public static void main(String[] args) {
-        char [] letra;
-        letra = new char[1];
-    }    
-}
+        char [] letra = {'T','R','W','G','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
+        letra = new char[23];
+        int DNI;
+        Scanner lector;
+        lector = new Scanner(System.in);
+
+        //Programa principal
+        System.out.println("Introduce tu dni: ");
+        DNI = lector.nextInt();
+
+        System.out.println("tu dni es "+DNI+letra[(DNI/23)]);
+        lector.close();
+
+    }
+}    
