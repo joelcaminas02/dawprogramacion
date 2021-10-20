6fa85ce94edc6cd151aeecf6b0caf81bb6d637db
joelcaminas02 <joelcg2002@gmail.com>
2021-10-20 09:36:29 +0200

    Ejercicio32Final

---
+++ tema 1/Ejercicio32.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Ejercicio32 {
+    public static void main(String[] args) {
+        //declaracion de variables
+        int i;
+        int [] anArray;
+        anArray = new int[10];
+        Scanner lector;
+        lector = new Scanner(System.in);
+
+        //Inicio programa
+        System.out.println("Introduce un numero: ");
+
+        for (i = 0; i<10; i++){
+            anArray[i] = lector.nextInt();
+        }
+
+        //numeros en orden inverso
+        for (int x = 9; x>=0 ;x--){
+            System.out.println(anArray[x]);
+        }
+        lector.close();
+    }    
+}
