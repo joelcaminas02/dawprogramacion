3e1e8c6b718d99335432e5718ad93f65b81aa73a
joelcaminas02 <joelcg2002@gmail.com>
2021-10-27 10:31:16 +0200

    Mates AreaCirculo

--- Tema2/Mates.java
+++ Tema2/Mates.java
@@ -11,10 +11,7 @@ public class Mates {
     public static double division (double x,double y){
         return x/y;
     }
-    public static void main(String[] args) {
-    System.out.println("2 + 3 = "+suma(2,3));
-    System.out.println("5,5-2,1 = "+resta(5.5,2.1));
-    System.out.println("4,2 * 2,3 = "+multiplicacion(4.7,2.3));
-    System.out.println("20,2 / 2,1 = "+division(20.2,2.1));    
+    public static double areaCirculo (double radio){
+        return Math.PI*Math.pow(radio, 2);
     }
 }
---
+++ Tema2/MatesPruebas.java
@@ -0,0 +1,9 @@
+public class MatesPruebas {
+    public static void main(String[] args) {
+        System.out.println("2 + 3 = "+Mates.suma(2,3));
+        System.out.println("5,5-2,1 = "+Mates.resta(5.5,2.1));
+        System.out.println("4,2 * 2,3 = "+Mates.multiplicacion(4.7,2.3));
+        System.out.println("20,2 / 2,1 = "+Mates.division(20.2,2.1));
+        System.out.println("El area de un circulo con radio 5,2 es:"+Mates.areaCirculo(5.2));    
+        }
+}
