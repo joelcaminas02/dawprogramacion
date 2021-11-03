ec1b2b8984af2dc1bedab1a6868d37897b3d0da8
joelcaminas02 <joelcg2002@gmail.com>
2021-11-03 09:13:17 +0100

    Ejercicio36

--- tema 1/Ejercicio36.java
+++ tema 1/Ejercicio36.java
@@ -5,15 +5,23 @@ public class Ejercicio36 {
         Scanner lector;
         lector = new Scanner(System.in);
 
-        int [] numeros;
-        numeros = new int [10];
+        double [] numeros;
+        numeros = new double [10];
+        double maximo = numeros[0];
 
         //Inicio programa
         System.out.println("Introduce 10 numeros");
 
-        for (int i = 0;0<10;i++){
+        for (int i = 0;i<numeros.length;i++){
             numeros[i]=lector.nextInt();
         }
         
+        for (int i = 1; i < numeros.length;i++){
+            if (numeros[i] > maximo){
+                maximo = numeros[i];
+            }
+        }
+        lector.close();
+        System.out.println(maximo);
     }
 }
