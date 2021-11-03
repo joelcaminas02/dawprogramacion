a554b6300b72cdf1de3499f73be37c81f515c7fc
joelcaminas02 <joelcg2002@gmail.com>
2021-11-03 08:46:38 +0100

    tema1

---
+++ tema 1/Ejercicio36.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Ejercicio36 {
+    public static void main(String[] args) {
+        //Declaracion de variables
+        Scanner lector;
+        lector = new Scanner(System.in);
+
+        int [] numeros;
+        numeros = new int [10];
+
+        //Inicio programa
+        System.out.println("Introduce 10 numeros");
+
+        for (int i = 0;0<10;i++){
+            numeros[i]=lector.nextInt();
+        }
+        
+    }
+}
