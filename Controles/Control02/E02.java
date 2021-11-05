9fe76fefac6b0d6d947af28da2c5818e714c4291
joelcaminas02 <joelcg2002@gmail.com>
2021-11-05 09:26:34 +0100

    E02 fin

---
+++ Controles/Control02/E02.java
@@ -0,0 +1,19 @@
+package Control02;
+
+import java.util.Scanner;
+
+public class E02 {
+    public static void main(String[] args) {
+        //Declracion de variables
+        Scanner lector;
+        lector = new Scanner(System.in);
+        
+        //Inicio Programa
+        System.out.println("Introduce el radio: ");
+        int radio = lector.nextInt();
+        
+        System.out.println("Perimetro: "+ (2*radio)*Math.PI);
+        System.out.println("Área: "+ (radio*radio)*Math.PI);
+        lector.close();
+    }
+}
