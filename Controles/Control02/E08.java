53eafaeb2ca8237bf34a55fcf54bea30ab993660
joelcaminas02 <joelcg2002@gmail.com>
2021-11-05 10:25:22 +0100

    E08.java Fin

---
+++ Controles/Control02/E08.java
@@ -0,0 +1,15 @@
+package Control02;
+
+import java.util.Scanner;
+
+public class E08 {
+    public static void main(String[] args) {
+		double numero;
+        Scanner lector;
+        lector = new Scanner(System.in);
+		System.out.println("Ingrese un numero");numero= lector.nextDouble(); 
+        lector.close();
+		double parteDecimal = numero % 1;
+		System.out.println(parteDecimal);
+	}
+}
