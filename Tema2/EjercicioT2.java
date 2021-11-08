cf0bc65c045848746728208e9ecb40bd954056cf
joelcaminas02 <joelcg2002@gmail.com>
2021-11-08 13:00:27 +0100

    Algo

---
+++ Tema2/EjercicioT2.java
@@ -0,0 +1,7 @@
+public class EjercicioT2 {
+    public static void main(String[] args) {
+        //Ejercicio EsDvisible
+        System.out.println(EsDivisible.esDivisible(4, 2));
+        //
+    }
+}
