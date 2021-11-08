38e88a5355b15068075813950df2a5cdc8db396b
joelcaminas02 <joelcg2002@gmail.com>
2021-11-08 13:00:35 +0100

    messi

---
+++ Tema2/EsDivisible.java
@@ -0,0 +1,9 @@
+public class EsDivisible {
+    public static boolean esDivisible(int n, int m){
+        boolean divisible = false;
+        if (n%m == 0){
+            divisible = true;
+        }
+        return divisible;
+    }
+}
