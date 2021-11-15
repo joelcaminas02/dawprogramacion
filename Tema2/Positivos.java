bc07375942de24f286e14eb59daa2045c6277b13
joelcaminas02 <joelcg2002@gmail.com>
2021-11-15 08:56:19 +0100

    positivos fin

--- Tema2/Positivos.java
+++ Tema2/Positivos.java
@@ -1,19 +1,21 @@
 import java.util.*;
 public class Positivos {
-    public static void Postives (){
-        int x;
-        Scanner lector;
+    public static void positivos (int [] x){
         int positivos = 0;
-        lector = new Scanner(System.in);
-        for (int i = 0;i<10;i++){
-            x = lector.nextInt();
-            if(x >= 0){
-                positivos++;
+        for (int i = 0;i<x[i];i++){
+        if(x[i] >= 0){
+            positivos++;
             }
-        }lector.close();
+        }
         System.out.println(positivos);
     }
     public static void main(String[] args) {
-        Postives();
-    }
+        int[] x = new int[10];
+        Scanner lector;
+        lector = new Scanner(System.in);
+        for (int i = 0;i<10;i++){
+            x[i]= lector.nextInt();
+    }lector.close();
+    positivos(x);
+}
 }
