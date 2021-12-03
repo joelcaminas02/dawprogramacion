b47d20a7ade786b1d59748723cfb90d9d770923d
joelcaminas02 <joelcg2002@gmail.com>
2021-12-03 09:08:19 +0100

    a

--- Tema3/MoverCeros.java
+++ Tema3/MoverCeros.java
@@ -1,21 +1,18 @@
 public class MoverCeros {
     public static void main(String[] args) {
         int [] array = {1,2,3,4,0,0,6,7,8,0,2,1};
-        int contador = 0;
-        for (int i = 0; i < array.length; i++){
-            if (array[i] == 0){
-                contador++;
+        for (int i = array.length-1; i >= 0; i--){
+            if ( array[i] == 0){
+                moverCeroDerecha(array, i);
             }
-        }
-        for (int i = 0 ; i < array.length;i++){
-            if (array[i] == 0){
-                array[i] = array[i+1];
-            }
-        }
-        for (int i = array.length-1; contador == 0; contador--){
-            array[i] = 0;
-            i--;
         }
         Utilidades.mostrarArray(array);
+    }
+
+    public static void moverCeroDerecha(int[] array, int pos) {
+        for (int i = pos; i < array.length; i++){
+            array[i] = array[i+1];
+            array[i+1] = 0;
+        }
     }
 }
--- Tema3/ValoresACero.java
+++ Tema3/ValoresACero.java
@@ -1,10 +1,12 @@
+import org.w3c.dom.css.CSS2Properties;
+
 public class ValoresACero {
     public static void main(String[] args) {
         int [][] matriz = new int [10][10];
-        Utilidades.rellenaMatriz(matriz, 0, 1);
+        Utilidades.rellenaMatriz(matriz, 0, 0);
         int filas = matriz.length;
         int columnas = matriz[0].length;
-        int columnas0= 0;int filas0= 0;
+        int columnas0= 0, filas0= 0, columnas00 = 0, filas00 = 0;
         for (int i = 0; i<filas;i++){
             for (int j = 0; j<columnas;j++){
                 System.out.print(matriz[i][j]); 
@@ -13,13 +15,25 @@ public class ValoresACero {
                 }
                 if (matriz[i][0] == 0){
                     columnas0++;
+                    if ( columnas0 == 10 && j == 9){
+                        columnas00++;
+                        columnas0 = 0;
+                    }else{
+                        columnas0 = 0; 
+                    }
                 }
                 if (matriz[i][j] == 0){
                     filas0++;
+                    if(filas0 == 10 && j == 9){
+                        filas00++;
+                        filas0 = 0;
+                    }else{
+                        filas0 = 0;
+                    }
                 }
             }
         }
-        System.out.println("Tiene "+columnas0+" a 0");
-        System.out.println("Tiene "+filas0+" a 0");
+        System.out.println("Tiene "+columnas00+" a 0");
+        System.out.println("Tiene "+filas00+" a 0");
     }
 }
