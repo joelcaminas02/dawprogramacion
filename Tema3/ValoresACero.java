8879eb2df16f891822bed1b3993e250bf83a11d3
joelcaminas02 <joelcg2002@gmail.com>
2021-12-10 08:48:40 +0100

    ValoresA0

--- Tema3/ValoresACero.java
+++ Tema3/ValoresACero.java
@@ -1,37 +1,37 @@
 public class ValoresACero {
     public static void main(String[] args) {
         int [][] matriz = new int [10][10];
-        Utilidades.rellenaMatriz(matriz, 0, 0);
+        Utilidades.rellenaMatriz(matriz, 0, 1);
         int filas = matriz.length;
         int columnas = matriz[0].length;
-        int columnas0= 0, filas0= 0, columnas00 = 0, filas00 = 0;
+        int Filas0= 0;
         for (int i = 0; i<filas;i++){
+            boolean esFilacero = true;
             for (int j = 0; j<columnas;j++){
-                System.out.print(matriz[i][j]); 
-                if (j == 9){
-                    System.out.println("");
-                }
-                if (matriz[i][0] == 0){
-                    columnas0++;
-                    if ( columnas0 == 10 && j == 9){
-                        columnas00++;
-                        columnas0 = 0;
-                    }else{
-                        columnas0 = 0; 
-                    }
-                }
-                if (matriz[i][j] == 0){
-                    filas0++;
-                    if(filas0 == 10 && j == 9){
-                        filas00++;
-                        filas0 = 0;
-                    }else{
-                        filas0 = 0;
-                    }
+                if ( matriz[i][j] == 1){
+                    esFilacero = false;
+                    break;
                 }
             }
+            if (esFilacero){
+                Filas0++;
+            }
         }
-        System.out.println("Tiene "+columnas00+" a 0");
-        System.out.println("Tiene "+filas00+" a 0");
+        System.out.println(Filas0);
+        
+        int Columna0 = 0;
+        for (int i = 0; i<filas;i++){
+            boolean esColumnacero = true;
+            for (int j = 0; j<columnas;j++){
+                if ( matriz[j][i] == 1){
+                    esColumnacero = false;
+                    break;
+                }
+            }
+            if (esColumnacero){
+                Columna0++;
+            }
+        }
+        System.out.println(Columna0);
     }
 }
