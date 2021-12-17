6a31cb9d823a4f28ae9ff63af25a4b137e1fe8a4
joelcaminas02 <joelcg2002@gmail.com>
2021-12-17 10:23:28 +0100

    Siglas acabdo

---
+++ Tema3/Strings/Siglas.java
@@ -0,0 +1,29 @@
+package Strings;
+
+public class Siglas {
+    public static String[] palabras (String frase){
+        frase=frase.trim();
+    	frase=frase.replaceAll(" +", " ");
+        String [] palabras = frase.split(" ");
+       
+        return palabras;
+    }
+
+    public static String Siglas (String[] palabras){
+        String resultado = "";
+        for(int i = 0; i<palabras.length; i++){
+            
+            resultado += palabras[i].charAt(0);
+            
+        }
+        return resultado;
+    }
+    public static void main(String[] args) {
+        String frase = "hola mundo jeje";
+        String []array = palabras(frase);
+
+        System.out.println(Siglas(array));
+
+   }
+
+}
