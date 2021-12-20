4ca8f4e9561667f2751d64d297909c462d3b1c71
joelcaminas02 <joelcg2002@gmail.com>
2021-12-20 10:25:07 +0100

    alfabeticamente

---
+++ Tema3/Strings/Pasatiempos.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class Pasatiempos {
+    public static void transformaApuntos (String cadena){
+        Utilidades.dividirEnPalabras(cadena);
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
