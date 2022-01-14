261bbe6709a3efd3ef0e532edff1d7431e457beb
joelcaminas02 <joelcg2002@gmail.com>
2022-01-14 10:42:24 +0100

    aa

--- Tema3/Objetos/Ejemplo1.java
+++
@@ -1,6 +0,0 @@
-/*crear clase alumno, cuyos objetos tendran como caracterisiticas un id, un nombre, y un grupo . Se pueden crear 
-alumnos solo con el nombre y el identificador o tambn con todo. Tiene que tener un metodo propio de los objetos
-que se llama preguntar duda que simplemente tiene que imprimir el "nombre del alumno "levanta la mano". Nuestra 
-clase tiene que llevar un contador del total de alumnos creados. tambn hay un metodo de clase llamado imprimir informe
-que muestra el total de alumnos. Otro metodo de clase que se llama "imprimir informe alumnos" al que se le pasa como parametro
-un array de alumnos eh imprime el nombre y grupo de cada alumno.*/
