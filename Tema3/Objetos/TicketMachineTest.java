95ece0faa72f7af1f54a520e8ea2ae7b608f4224
joelcaminas02 <joelcg2002@gmail.com>
2022-01-21 10:18:07 +0100

    tiket finalizado

---
+++ Tema3/Objetos/TicketMachineTest.java
@@ -0,0 +1,15 @@
+package Objetos;
+
+public class TicketMachineTest {
+    public static void main(String[] args) {
+        TicketMachine tm1 = new TicketMachine(100);
+        tm1.prompt();
+        tm1.insertMoney(100);
+        tm1.insertMoney(200);
+        tm1.printTicket();
+        tm1.showPrice();
+        tm1.empty();
+        tm1.showPrice();
+
+    }
+}
