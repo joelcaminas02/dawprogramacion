78b146a651c9084052be26f9ac020805a04a37bd
joelcaminas02 <joelcg2002@gmail.com>
2022-05-09 08:47:51 +0200

    json

--- Controles/Control04/PersistenciaTest.java
+++ Controles/Control04/PersistenciaTest.java
@@ -7,16 +7,18 @@ public class PersistenciaTest {
     public static void main(String[] args) throws IOException, ClassNotFoundException,ParserConfigurationException,TransformerException {
         File fichero = new File("clientes.dat");
         File ficheroObjects = new File("clientesObjetos.dat");
+        File ficheroJSON = new File ("fichero.json");
         Cliente cl1 =  new Cliente("joel@pepo", "12345678k", "Joel", "Carrillero");
         Cliente cl2 =  new Cliente("jose@pepo", "12348978k", "Juan", "Calvo");
         Cliente cl3 =  new Cliente("juan@pepo", "12341278k", "Pepe", "Sendo");
         Cliente cl4 =  new Cliente("pepe@pepo", "12344378k", "Mesi", "Kbron");
         Cliente cl5 =  new Cliente("aaaa@pepo", "12344378k", "Jose", "Carri");
 
-        //PersistenciaCliente.write(Cliente.clientes);
+        PersistenciaCliente.write(Cliente.clientes);
 
         PersistenciaCliente.writeObjects(Cliente.clientes);
         PersistenciaCliente.crearXML(ficheroObjects);
-        //PersistenciaCliente.read(fichero);
+        PersistenciaCliente.crearJSON(ficheroJSON);
+        PersistenciaCliente.read(fichero);
     }
 }
